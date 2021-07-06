9fca0d00321cb2605347b300c0d80dad16f237a9
max-ringel <max@dot9.co>
2021-07-07 00:35:23 +0200

    wip: localization about; home

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Footer from "../partials/Footer";
 import Header from "../partials/Header";
+import { useTranslation, Trans } from "react-i18next";
 
 function AboutUs() {
+
+  const { t } = useTranslation();
+
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
       {/*  Site header */}
@@ -22,15 +26,11 @@ function AboutUs() {
                   className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4 bg-clip-text text-transparent bg-gradient-to-r from-d9Blue-900 to-d9Blue-900"
                   data-aos="zoom-y-out"
                 >
-                  About Us
+                  {t('about-us-title')}
                 </h1>
                 <div className="max-w-xl mx-auto">
                   <p className="text-xl text-gray-600 mb-2">
-                    We're a 5-person team combining 20+ years of industry
-                    experience to design and implement whatever software you
-                    want, truly customized to your needs. Caring for our clients
-                    is at the core of our philosophy, and if you're not happy,
-                    we aren't either.
+                    {t('about-us-text')}
                   </p>
                 </div>
               </div>
@@ -40,7 +40,7 @@ function AboutUs() {
                 className="text-center text-3xl md:text-3xl font-extrabold leading-tighter tracking-tighter mb-4 bg-clip-text text-transparent bg-gradient-to-r from-d9Blue-900 to-d9Blue-900"
                 data-aos="zoom-y-out"
               >
-                Our Founders
+                {t('founders-section-title')}
               </h1>
               <div className="flex justify-between mt-10 text-center">
                 <div className="flex-col max-w-md">
@@ -55,16 +55,14 @@ function AboutUs() {
                     Felix Wu
                   </span>
                   <p className="text-xl text-gray-600">
-                    A Full-Stack Web and Infrastructure expert with {`>`}5 years
-                    experience. Currently Senior Software Engineer at{" "}
+                    <Trans i18nKey="felix-wu-description">
                     <a className="underline" href="https://google.com">
-                      Google
                     </a>
-                    , working on Release Management for GCP. Based in Zurich.
+                    </Trans>
                   </p>
                 </div>
                 <div className="flex-col max-w-md">
-                  
+
                   <img
                     className="md:max-w-none mx-auto rounded-full mt-10 mb-4"
                     src={require("../images/christof_2.png").default}
@@ -76,13 +74,10 @@ function AboutUs() {
                     Christof Zirkler
                   </span>
                   <p className="text-xl text-gray-600">
-                    A Mobile and Full-Stack expert with {`>`}10 years
-                    experience. Currently Mobile Team Lead at
+                    <Trans i18nKey="christof-zirkler-description">
                     <a className="underline" href="https://livingpackets.com">
-                      {" "}
-                      LivingPackets
                     </a>
-                    , an upcoming logistics startup. Based in Berlin.
+                    </Trans>
                   </p>
                 </div>
               </div>
