08adec17e6cae38006c97f90debc87bc98d4c354
max-ringel <max@dot9.co>
2021-07-07 01:27:24 +0200

    feat: auto-browser-language detection

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,9 +1,9 @@
 import i18n from 'i18next';
+import detector from "i18next-browser-languagedetector";
 import { initReactI18next } from 'react-i18next';
 
-i18n.use(initReactI18next).init({
+i18n.use(detector).use(initReactI18next).init({
   fallbackLng: 'en',
-  lng: 'de',
   resources: {
     en: {
       translations: require('./locales/en/translations.json')
