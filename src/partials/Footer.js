7cde7f7d05450de4ea1dcac11196e06a6efe7ac7
Christof Zirkler <zirklerchristof@gmail.com>
2021-08-18 18:37:26 +0200

    feat(Footer): add career link

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -11,10 +11,12 @@ function Footer() {
   return (
     <footer>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
+
         {/* Top area: Blocks */}
-        <div className="flex md:py-12 border-t border-gray-200 justify-between pt-8 pb-8">
-          {/* 1st block */}
-          <div className="flex flex-col">
+        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
+          
+          {/* Logo Block */}
+          <div className="sm:col-span-12 lg:col-span-3">
             <div className="mb-2">
               <Logo width="80" height="40" />
             </div>
@@ -35,32 +37,48 @@ function Footer() {
             </div>
           </div>
 
-          {/* Schedule Video Call Button */}
-          <div className="flex flex-col">
-            <a
-              className="btn text-white bg-d9Blue-900 hover:bg-d9Blue-800 shadow"
-              href="https://dot9.appointlet.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <svg
-                className="h-6 w-6 mr-2"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                ></path>
-              </svg>
-              {t('schedule-video-call-button')}
-            </a>
+          {/* Company Block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">{t('footer-company-block-title')}</h6>
+            <ul className="text-sm">
+              <li className="mb-2">
+                <Link to="/" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">{t('nav-home')}</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="/about" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">{t('nav-about')}</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="/careers" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">{t('nav-careers')}</Link>
+              </li>
+            </ul>
           </div>
+
+          {/* Schedule Video Call Button */}
+          <div className="sm:col-span-12 md:col-span-12 lg:col-span-7 lg:justify-self-end">
+          <a
+            className="btn text-white bg-d9Blue-900 hover:bg-d9Blue-800 shadow"
+            href="https://dot9.appointlet.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <svg
+              className="h-6 w-6 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+              ></path>
+            </svg>
+            {t('schedule-video-call-button')}
+          </a>
+        </div>
         </div>
 
         {/* Bottom area */}
